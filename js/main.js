9b81da59ea83cbef4b2f3f9e0d9804f57fe7a7be
alvarogarcia1010 <00111317@uca.edu.sv>
2018-12-07 23:12:21 -0600

    Implementando el tema de bootstrap 4

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
   $('#calendar').fullCalendar({
     locale: 'es',
+    themeSystem: 'bootstrap4',
+    header: {
+            left:   'prev,next today',
+            center: 'title',
+            right:  'month,agendaWeek,agendaDay,listMonth'
+            },
+    // events: 'https://fullcalendar.io/demo-events.json',
     schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives'
   });
 
